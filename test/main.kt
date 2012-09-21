2b26dd3c52c97369b960403e2a784ac251a7eaa9
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-21 22:32:20 +0400

    Simple visualisation demo added.

---
+++ test/main.kt
@@ -0,0 +1,48 @@
+import junit.framework.TestCase
+import org.jetbrains.kannotator.controlFlow.ControlFlowGraphBuilder
+import org.jetbrains.kannotator.controlFlow.InstructionMetadata
+import edu.uci.ics.jung.graph.Graph
+import edu.uci.ics.jung.graph.SparseMultigraph
+import edu.uci.ics.jung.graph.util.EdgeType
+import edu.uci.ics.jung.graph.DirectedOrderedSparseMultigraph
+import org.jetbrains.kannotator.controlFlow.Instruction
+import org.jetbrains.kannotator.controlFlow.ControlFlowEdge
+import edu.uci.ics.jung.graph.DirectedSparseMultigraph
+import edu.uci.ics.jung.algorithms.layout.CircleLayout
+import java.awt.Dimension
+import edu.uci.ics.jung.visualization.BasicVisualizationServer
+import javax.swing.JFrame
+
+
+fun main(args: Array<String>) {
+    val builder = ControlFlowGraphBuilder<String>()
+    val a = builder.newInstruction(StringInstructionMetadata("a"))
+    val b = builder.newInstruction(StringInstructionMetadata("b"))
+    val c = builder.newInstruction(StringInstructionMetadata("c"))
+    builder.setEntryPoint(a)
+    builder.addEdge(a, b)
+    builder.addEdge(b, c)
+    builder.addEdge(c, a)
+    val graph = builder.build()
+
+    val jungGraph = DirectedSparseMultigraph<Instruction, ControlFlowEdge>()
+    for (i in graph.instructions) {
+        for (e in i.outgoingEdges) {
+            jungGraph.addEdge(e, e.from, e.to, EdgeType.DIRECTED)
+        }
+    }
+
+    val layout = CircleLayout(jungGraph);
+    layout.setSize(Dimension(300, 300)); // sets the initial size of the space
+    // The BasicVisualizationServer<V,E> is parameterized by the edge types
+    val vv = BasicVisualizationServer(layout);
+    vv.setPreferredSize(Dimension(350,350)); //Sets the viewing area size
+
+    val frame = JFrame("Simple Graph View");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane()?.add(vv);
+    frame.pack();
+    frame.setVisible(true);
+}
+
+private data class StringInstructionMetadata(val data: String): InstructionMetadata {}
