0d266614666032f5591e9b48b82491a10cf45236
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-24 13:58:14 +0400

    Pretties opcode rendering

--- src/org/jetbrains/kannotator/asm/util/asmUtil.kt
+++ src/org/jetbrains/kannotator/asm/util/asmUtil.kt
@@ -2,11 +2,21 @@ package org.jetbrains.kannotator.asm.util
 
 import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.util.Printer
+import org.objectweb.asm.tree.LineNumberNode
+import org.objectweb.asm.tree.LabelNode
 
 public fun AbstractInsnNode.toOpcodeString(): String {
-    val opcode = getOpcode()
-    if (opcode == -1) {
-        return "No opcode: " + javaClass.getSimpleName()
+    return when (this) {
+        is LineNumberNode -> "Line " + line
+        is LabelNode -> "Label: ${getLabel()}"
+        else -> {
+            val opcode = getOpcode()
+            if (opcode == -1) {
+                "No opcode: " + javaClass.getSimpleName()
+            }
+            else {
+                Printer.OPCODES[opcode]!!
+            }
+        }
     }
-    return Printer.OPCODES[opcode]!!
 }
