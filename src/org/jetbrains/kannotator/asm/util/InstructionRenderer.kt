2256dac7acfcddd83c11faa58ed2b8f451acb4e2
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-24 21:49:29 +0400

    InstructionRenderer: deterministic label names and such

---
+++ src/org/jetbrains/kannotator/asm/util/InstructionRenderer.kt
@@ -0,0 +1,35 @@
+package org.jetbrains.kannotator.asm.util
+
+import org.objectweb.asm.Label
+import java.util.HashMap
+import org.objectweb.asm.tree.AbstractInsnNode
+import org.objectweb.asm.util.Textifier
+import org.objectweb.asm.util.TraceMethodVisitor
+import kotlinlib.*
+import org.objectweb.asm.tree.LabelNode
+import org.objectweb.asm.tree.LineNumberNode
+import org.objectweb.asm.tree.JumpInsnNode
+import org.objectweb.asm.util.Printer
+
+public class AsmInstructionRenderer {
+    private val labelNames: MutableMap<Label, String> = HashMap()
+
+    public fun labelName(label: Label): String {
+        return labelNames.getOrPut(label) {"L" + labelNames.size()}
+    }
+
+    public fun render(insn: AbstractInsnNode): String {
+        if (insn is LabelNode) {
+            return labelName(insn.getLabel())
+        }
+        if (insn is LineNumberNode) {
+            return "LINENUMBER ${insn.line} ${labelName(insn.start.getLabel())}"
+        }
+        if (insn is JumpInsnNode) {
+            return Printer.OPCODES[insn.getOpcode()] + " ${labelName(insn.label.getLabel())}"
+        }
+        val textifier = Textifier()
+        insn.accept(TraceMethodVisitor(textifier))
+        return textifier.getText().map {x -> x.toString().trim()}.join(" ")
+    }
+}
