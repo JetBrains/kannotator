6dc43a6ee7bab6b505b5019ae530f52335dc9e6c
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-24 21:46:31 +0400

    indexed: working around a bug in Kotlin + making it more efficient in theory (once we cache function instances)

--- kotlinlib/kotlinlib/indexed.kt
+++ kotlinlib/kotlinlib/indexed.kt
@@ -6,22 +6,23 @@ public data class IndexedElement<T>(
         public val value: T
 )
 
-public inline val <T> Array<T>.indexed: Iterator<IndexedElement<T>>
-        get() {
-            val array = this
-            return object : Iterator<IndexedElement<T>> {
-                var index = 0
-                val maxIndex = array.size - 1
+public fun <T, C> indexedIterator(collection: C, size: Int, get: (C, Int) -> T): Iterator<IndexedElement<T>> {
+    return object : Iterator<IndexedElement<T>> {
+        var index = 0
+        val maxIndex = size - 1
 
-                override fun next(): IndexedElement<T> {
-                    return IndexedElement(index, array[index++])
-                }
-
-                override fun hasNext(): Boolean {
-                    return index <= maxIndex
-                }
-            }
+        override fun next(): IndexedElement<T> {
+            return IndexedElement(index, get(collection, index++))
         }
+
+        override fun hasNext(): Boolean {
+            return index <= maxIndex
+        }
+    }
+}
+
+public inline val <T> Array<T>.indexed: Iterator<IndexedElement<T>>
+        get() = indexedIterator(this, this.size) { c, i -> c[i] }
 
 public inline val <T> Iterator<T>.indexed: Iterator<IndexedElement<T>>
     get() {
