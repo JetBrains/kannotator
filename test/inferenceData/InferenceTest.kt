1cba7afa03a24b2cfe5e56cb523de45927f5e959
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2012-09-25 19:18:48 +0400

    tests for nullability inference

---
+++ test/inferenceData/InferenceTest.kt
@@ -0,0 +1,26 @@
+package inference
+
+import junit.framework.TestCase
+import org.objectweb.asm.ClassReader
+import org.jetbrains.kannotator.annotationsInference.inferAnnotations
+import org.jetbrains.kannotator.controlFlowBuilder.buildControlFlowGraph
+import org.jetbrains.kannotator.nullability.NullabilityValueInfo
+import org.jetbrains.kannotator.nullability.NullabilityValueInfo.*
+import kotlin.test.assertEquals
+
+class InferenceTest: TestCase() {
+    fun doTest(theClass: Class<out Any>, methodName: String, methodDescriptor: String, expected: List<NullabilityValueInfo>) {
+        val classReader = ClassReader(theClass.getCanonicalName())
+        val graph = buildControlFlowGraph(classReader, methodName, methodDescriptor)
+        val result = inferAnnotations(graph)
+        assertEquals(expected, result)
+    }
+
+    fun testNull() = doTest(javaClass<inferenceData.Test>(), "testNull", "()Ljava/lang/Object;", arrayList(NULL))
+
+    fun testNullOrObject() = doTest(javaClass<inferenceData.Test>(), "testNullOrObject", "()Ljava/lang/Object;", arrayList(NULLABLE))
+
+    fun testNotNullParameter() = doTest(javaClass<inferenceData.Test>(), "testNotNullParameter", "(Ljava/lang/String;)V", arrayList(UNKNOWN, NOT_NULL));
+
+//    fun testInvocationOnCheckedParameter() = doTest(javaClass<inferenceData.Test>(), "testInvocationOnCheckedParameter", "(Ljava/lang/String;)V", arrayList(UNKNOWN, UNKNOWN));
+}
---
+++ testData/inferenceData/Test.java
@@ -0,0 +1,25 @@
+package inferenceData;
+
+public class Test {
+    public Object testNull() {
+        return null;
+    }
+
+    public Object testNullOrObject() {
+        if ("abc".getBytes().length == 0)
+            return new Object();
+        else
+            return null;
+    }
+
+    public void testNotNullParameter(String a) {
+        a.getBytes();
+    }
+
+    public void testInvocationOnCheckedParameter(String a) {
+        if (a != null) {
+            a.getBytes();
+        }
+    }
+
+}
