c1c919d85d98b103d31fc6cf4df3e2b16f35eb1a
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2012-09-25 19:17:55 +0400

    introduced NullabilityValueInfo enum merge for NullabilityValueInfo

--- src/org/jetbrains/kannotator/nullability/NullabilityValueInfo.kt
+++ src/org/jetbrains/kannotator/nullability/NullabilityValueInfo.kt
@@ -1,13 +1,30 @@
 package org.jetbrains.kannotator.nullability
 
-public trait NullabilityValueInfo {
-    val mayBeNull: Boolean
-    val mayBeNotNull: Boolean
+import kotlin.nullable.fold
+
+public enum class NullabilityValueInfo() {
+    NOT_NULL
+    NULL
+    NULLABLE
+    CONFLICT
+    UNKNOWN
+
+    // can be cached
+    fun merge(that: NullabilityValueInfo): NullabilityValueInfo {
+        if (this == that) return this
+
+        if (this == CONFLICT) return that
+        if (that == CONFLICT) return this
+
+        if (this == NULL || that == NULL) return NULLABLE
+
+        if (this == NULLABLE || that == NULLABLE) return NULLABLE
+
+        return UNKNOWN // NOT_NULL + UNKNOWN
+    }
 }
 
-val NullabilityValueInfo.definitelyNull: Boolean
-    get() = mayBeNull && !mayBeNotNull
-
-val NullabilityValueInfo.definitelyNotNull: Boolean
-    get() = mayBeNotNull && !mayBeNull
-
+fun Iterable<NullabilityValueInfo>.merge() : NullabilityValueInfo {
+    if (!iterator().hasNext()) return NullabilityValueInfo.UNKNOWN
+    return this.fold(NullabilityValueInfo.CONFLICT, { (res : NullabilityValueInfo, value) -> res merge value} )
+}
