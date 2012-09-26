8cdaa0104b267ed89268b3d36e8fdd5d05af7ddf
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-09-26 17:14:00 +0400

    FunDependencyGraph: output of number of components in tests

--- test/funDependency/BuildGraphForLibrariesTest.kt
+++ test/funDependency/BuildGraphForLibrariesTest.kt
@@ -9,21 +9,32 @@ import org.objectweb.asm.ClassReader
 import util.processJar
 
 class BuildGraphForLibrariesTest() {
-    test fun test() {
+    test fun allLibsTest() {
         File("lib").recurse {
             file ->
             if (file.isFile() && file.getName().endsWith(".jar")) {
-                println("Processing: $file")
-
-                val classReaders = ArrayList<ClassReader>()
-                processJar(file,  {
-                    jarFile, classType, classReader ->
-                    classReaders.add(classReader)
-                })
-
-                val graph = buildFunctionDependencyGraph(classReaders)
-                println("Graph size: " + graph.functions.size() + " " + graph.noOutgoingNodes.size())
+                doTest(file)
             }
         }
     }
+
+    /*test fun rtJarTest() {
+       doTest(File("c:/Program Files/Java/jdk1.6.0_30/jre/lib/rt.jar"))
+    }*/
+
+    fun doTest(file: File) {
+        println("Processing: $file")
+        val classReaders = ArrayList<ClassReader>()
+        processJar(file, {
+            jarFile, classType, classReader ->
+            classReaders.add(classReader)
+        })
+
+        val graph = buildFunctionDependencyGraph(classReaders)
+        val finder = SCCFinder(graph, { graph.functions }, { it.outgoingEdges.map { edge -> edge.to } })
+        val allComponents = finder.getAllComponents()
+        println("Graph size: " + graph.functions.size() + " " + graph.noOutgoingNodes.size())
+        println("Number of component (size > 1): ${allComponents.filter { it.size() > 1 }.size()}")
+    }
+
 }
