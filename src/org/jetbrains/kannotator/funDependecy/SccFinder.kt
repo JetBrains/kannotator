03ea2558276599c24e786b30bce3bf25efeb03a8
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-09-26 15:27:23 +0400

    Strong components finder

---
+++ src/org/jetbrains/kannotator/funDependecy/SccFinder.kt
@@ -0,0 +1,110 @@
+package funDependency
+
+import java.util.HashMap
+import java.util.HashSet
+import java.util.IdentityHashMap
+import java.util.LinkedHashSet
+import java.util.Stack
+import java.util.ArrayList
+import com.gs.collections.impl.set.strategy.mutable.MutableHashingStrategySetFactoryImpl
+import com.gs.collections.api.block.HashingStrategy
+
+/**
+ * Tarjan's strongly connected components algorithm
+ * http://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+ */
+public class SCCFinder<Graph, Node>(
+        val graph: Graph,
+        val graphNodes: (Graph) -> Iterable<Node>,
+        val adjacentNodes: (Node) -> Iterable<Node>) {
+
+    private class object {
+        private fun <T>identityHashSet(): MutableSet<T> = MutableHashingStrategySetFactoryImpl().with(object : HashingStrategy<T> {
+            public override fun equals(object1: T?, object2: T?): Boolean = object1 identityEquals object2
+            public override fun computeHashCode(`object`: T?): Int = System.identityHashCode(`object`)
+        })!!
+    }
+
+    private class IdentityHashStack<T> {
+        val stack = Stack<T>()
+        val identitySet = identityHashSet<T>()
+
+        fun contains(elem : T) : Boolean = identitySet.contains(elem)
+        fun push(elem : T) {
+            stack.push(elem)
+            assert(identitySet.add(elem))
+        }
+        fun pop() : T {
+            val elem = stack.pop()!!
+            assert(identitySet.remove(elem))
+            return elem
+        }
+        fun isEmpty() : Boolean {
+            assert(stack.isEmpty() == identitySet.isEmpty())
+            return stack.isEmpty()
+        }
+    }
+
+    private val components = ArrayList<Set<Node>>()
+    private val nodeToComponent = IdentityHashMap<Node, Set<Node>>()
+
+    // Index of visited nodes
+    private val nodeIndex = IdentityHashMap<Node, Int>()
+
+    // Current index for not-visited node
+    private var index = 0
+
+    public fun findComponent(node: Node): Set<Node> {
+        if (!nodeIndex.containsKey(node)) {
+            execute(node)
+        }
+
+        return nodeToComponent[node] ?: throw IllegalStateException("Can't find component for node ${node}")
+    }
+
+    private fun execute(node: Node) {
+        val stack = IdentityHashStack<Node>();
+        val minUnvisitedReachable = IdentityHashMap<Node, Int>()
+
+        executeOnNode(node, stack, minUnvisitedReachable)
+
+        assert(stack.isEmpty())
+    }
+
+    private fun executeOnNode(node: Node, stack: IdentityHashStack<Node>, minUnvisitedReachable: MutableMap<Node, Int>) {
+        val currentNodeIndex = index
+
+        minUnvisitedReachable[node] = currentNodeIndex
+        nodeIndex[node] = currentNodeIndex
+
+        index++
+
+        stack.push(node);
+
+        for (nextNode in adjacentNodes(node)) {
+            val currentComponentIndex = minUnvisitedReachable[node]!!
+
+            if (!nodeIndex.containsKey(nextNode)) {
+                executeOnNode(nextNode, stack, minUnvisitedReachable);
+                minUnvisitedReachable[node] = Math.min(currentComponentIndex, minUnvisitedReachable[nextNode]!!)
+            } else if (stack.contains(nextNode)) {
+                minUnvisitedReachable[node] = Math.min(currentComponentIndex, nodeIndex[nextNode]!!)
+            }
+        }
+
+        if (minUnvisitedReachable[node] == currentNodeIndex){
+            val component = identityHashSet<Node>()
+            do {
+                val componentNode = stack.pop()
+                component.add(componentNode)
+                nodeToComponent[componentNode] = component
+
+                if (componentNode identityEquals node) {
+                    break
+                }
+            } while(true)
+
+            components.add(component)
+        }
+    }
+}
---
+++ test/funDependency/SCCFinderTest.kt
@@ -0,0 +1,102 @@
+package funDependency
+
+import java.util.HashMap
+import java.util.HashSet
+import org.junit.Test
+import org.junit.Assert
+import com.sun.org.apache.xml.internal.utils.StringComparable
+import java.util.Comparator
+
+data class Node(val name: String)
+
+class Graph {
+    private val nodes = HashMap<String, Node>()
+    private val outgoingEdges = HashMap<Node, MutableSet<Node>>()
+
+    public fun getAllNodes(): List<Node> = nodes.values().toList()
+    public fun getAdjacentNodes(n: Node): Set<Node> = outgoingEdges[n]!!
+    public fun getNode(label: String): Node = nodes[label]!!
+    public fun getNodes(vararg labels: String): List<Node> = labels.map { getNode(it) }
+
+    fun nodes(vararg labels: String): Graph {
+        for (label in labels) {
+            node(label)
+        }
+
+        return this
+    }
+
+    fun node(label: String): Node {
+        val n = Node(label)
+        outgoingEdges[n] = HashSet()
+        nodes[label] = n
+        return n
+    }
+
+    fun edges(vararg fromToStr: String): Graph {
+        for (fromToEdgeStr in fromToStr) {
+            val split = fromToEdgeStr.split("\\-\\>")
+            val fromLabel = split[0].trim()
+            val toLabel = split[1].trim()
+
+            edge(nodes[fromLabel]!!, nodes[toLabel]!!)
+        }
+
+        return this
+    }
+
+    fun edge(from: Node, to: Node) {
+        outgoingEdges[from]!!.add(to)
+    }
+}
+
+class SCCFinderTest {
+    private val nodesComparator = object : Comparator<Node> {
+        public override fun compare(o1: Node?, o2: Node?): Int {
+            return o1?.name?.compareTo(o2?.name ?: "") ?: -1
+        }
+        public override fun equals(obj: Any?): Boolean {
+            throw UnsupportedOperationException()
+        }
+    }
+
+    Test fun test1() {
+        val graph = Graph().nodes("A", "B", "C").edges("A->B", "B->C", "C->B")
+        doTest(graph, "A->A", "B->B,C", "C->B,C")
+    }
+
+    Test fun test2() {
+        val graph = Graph().nodes("A", "B", "C").edges("A->B", "C->C")
+        doTest(graph, "A->A", "B->B", "C->C")
+    }
+
+    Test fun test3() {
+        val graph = Graph().nodes("A", "B", "C").edges("A->B", "B->C", "C->A")
+        doTest(graph, "A->A,B,C", "B->A,B,C", "C->A,B,C")
+    }
+
+    Test fun test4() {
+        val graph = Graph().nodes("A", "B", "C", "D").edges("A->B", "B->C", "B->D", "C->A", "C->D")
+        doTest(graph, "A->A,B,C", "B->A,B,C", "C->A,B,C", "D->D")
+    }
+
+    fun doTest(graph : Graph, vararg componentAssertion : String) {
+        val finder = SCCFinder<Graph, Node>(graph, { graph.getAllNodes() }, { graph.getAdjacentNodes(it).toList() })
+
+        fun assertInComponent(component : String) {
+            val split = component.split("\\-\\>")
+            val requestNode = graph.getNode(split[0].trim())
+            val nodes = graph.getNodes(*split[1].split("\\,").map { it.trim() }.toArray(array<String>()))
+
+            val componentNodes = finder.findComponent(requestNode)
+
+            Assert.assertArrayEquals("Invalid component nodes set for ${requestNode} node",
+                    nodes.sort(nodesComparator).toArray(),
+                    componentNodes.sort(nodesComparator).toArray())
+        }
+
+        for (component in componentAssertion) {
+            assertInComponent(component)
+        }
+    }
+}
