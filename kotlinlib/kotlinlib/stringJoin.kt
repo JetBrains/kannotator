a35826d88b4f0a7fc92fd6626468296d2c0c7966
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-27 17:48:53 +0400

    "before" and "after" for join()

--- kotlinlib/kotlinlib/stringJoin.kt
+++ kotlinlib/kotlinlib/stringJoin.kt
@@ -1,26 +1,31 @@
 package kotlinlib
 
-public fun Iterator<Any?>.join(separator: String = ""): String {
+public fun Iterator<Any?>.join(separator: String = "", before: String = "", after: String = ""): String {
     return buildString {
+        it.append(before)
         while (hasNext()) {
             it.append(next())
             if (hasNext()) {
                 it.append(separator)
             }
         }
+        it.append(after)
     }
 }
 
-public fun Iterable<Any?>.join(separator: String = ""): String = iterator().join(separator)
+public fun Iterable<Any?>.join(separator: String = "", before: String = "", after: String = ""): String
+        = iterator().join(separator, before, after)
 
-public fun Array<out Any?>.join(separator: String = ""): String {
+public fun Array<out Any?>.join(separator: String = "", before: String = "", after: String = ""): String {
     return buildString {
+        it.append(before)
         for (i in this.indices) {
             it.append(this[i])
             if (i < size - 1) {
                 it.append(separator)
             }
         }
+        it.append(after)
     }
 }
 
