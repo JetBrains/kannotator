848dc13c55c4f3f0ec9b8a438fb1153f2af11a81
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-27 17:55:09 +0400

    Initial version of type position serializer

---
+++ src/org/jetbrains/kannotator/annotations/io/typePositionSerializer.kt
@@ -0,0 +1,75 @@
+package org.jetbrains.kannotator.annotations.io
+
+import kotlinlib.join
+import org.jetbrains.kannotator.declarations.ClassName
+import org.jetbrains.kannotator.declarations.Method
+import org.jetbrains.kannotator.declarations.ParameterPosition
+import org.jetbrains.kannotator.declarations.PositionWithinMethod
+import org.jetbrains.kannotator.declarations.RETURN_TYPE
+import org.jetbrains.kannotator.declarations.TypePosition
+import org.jetbrains.kannotator.declarations.getArgumentTypes
+import org.jetbrains.kannotator.declarations.getReturnType
+import org.jetbrains.kannotator.declarations.isStatic
+import org.objectweb.asm.Type
+import org.objectweb.asm.signature.SignatureReader
+import org.objectweb.asm.util.TraceSignatureVisitor
+
+// ownerFQN returnType? name(paramType{", "}?) position?
+// Example:
+// org.objectweb.asm.ClassVisitor org.objectweb.asm.FieldVisitor visitField(int, java.lang.String, java.lang.String, java.lang.String, java.lang.Object)
+fun TypePosition.toAnnotationKey(): String {
+    return method.toAnnotationKeyPrefix() + positionWithinMethod.toAnnotationKeySuffix(method)
+}
+
+private fun Method.toAnnotationKeyPrefix(): String {
+    return declaringClass.canonicalName + " " +
+           this.getAnnotationKeyReturnTypeString() +
+           id.methodName + parameterTypesString()
+}
+
+private fun Method.getAnnotationKeyReturnTypeString(): String
+        = if (id.methodName == "<init>")
+            ""
+          else if (genericSignature != null) {
+                parseGenericSignature(genericSignature).getReturnType()!!.toCanonical() + " "
+          }
+          else canonicalName(getReturnType()) + " "
+
+private fun String.internalNameToCanonical(): String = replace('/', '.').toCanonical()
+
+private val ClassName.canonicalName: String
+    get() = internal.internalNameToCanonical()
+
+private fun String.toCanonical(): String = replace('$', '.')
+
+private fun canonicalName(_type: Type): String {
+    return _type.getClassName()?.toCanonical() ?: "!null"
+}
+
+private fun PositionWithinMethod.toAnnotationKeySuffix(method: Method): String {
+    return when (this) {
+        RETURN_TYPE -> ""
+        is ParameterPosition -> " " + correctIfNotStatic(method, this.index)
+        else -> throw IllegalArgumentException("Unknown position: $this")
+    }
+}
+
+private fun correctIfNotStatic(method: Method, parameterIndex: Int): Int {
+    // 'this' has index 0
+    return if (method.isStatic()) parameterIndex else parameterIndex - 1
+}
+
+private fun Method.parameterTypesString(): String {
+    if (genericSignature == null) {
+        return (id.getArgumentTypes() map {it -> canonicalName(it) }).join(",", "(", ")")
+    }
+    else {
+        return parseGenericSignature(genericSignature).getDeclaration().toCanonical()
+    }
+}
+
+private fun parseGenericSignature(signature: String): TraceSignatureVisitor {
+    val traceVisitor = TraceSignatureVisitor(0)
+    SignatureReader(signature).accept(traceVisitor)
+    return traceVisitor
+}
---
+++ test/annotations/io/PositionSerializerTest.kt
@@ -0,0 +1,79 @@
+package annotations.io
+
+import junit.framework.TestCase
+import org.jetbrains.kannotator.declarations.Method
+import org.jetbrains.kannotator.declarations.PositionWithinMethod
+import org.jetbrains.kannotator.declarations.TypePosition
+import org.jetbrains.kannotator.declarations.RETURN_TYPE
+import org.jetbrains.kannotator.declarations.Access
+import org.jetbrains.kannotator.declarations.ClassName
+import org.jetbrains.kannotator.annotations.io.toAnnotationKey
+import junit.framework.Assert.*
+import org.objectweb.asm.Opcodes
+import org.jetbrains.kannotator.declarations.ParameterPosition
+
+class PositionSerializerTest : TestCase() {
+    fun doTest(expected: String, owner: String, methodName: String, desc: String, position: PositionWithinMethod, signature: String? = null, static: Boolean = false) {
+        val access = if (static) Opcodes.ACC_STATIC else 0
+        val method = Method(ClassName.fromInternalName(owner), access, methodName, desc, signature)
+        val pos = MockTypePosition(method, position)
+        assertEquals(expected, pos.toAnnotationKey())
+    }
+
+    fun testVoid() {
+        doTest("a.b.C void foo()",
+               "a/b/C", "foo", "()V", RETURN_TYPE)
+    }
+
+    fun testClass() {
+        doTest("a.b.C a.b.C foo()",
+                "a/b/C", "foo", "()La/b/C;", RETURN_TYPE)
+    }
+
+    fun testInnerClass() {
+        doTest("a.b.C a.b.C.D foo()",
+                "a/b/C", "foo", "()La/b/C\$D;", RETURN_TYPE)
+    }
+
+    fun testStaticFirstParam() {
+        doTest("A void foo(int) 0",
+                "A", "foo", "(I)V", ParameterPosition(0), null, true)
+    }
+
+    fun testNonStaticFirstParam() {
+        doTest("A void foo(int) 0",
+                "A", "foo", "(I)V", ParameterPosition(1), null, false)
+    }
+
+    fun testGenericSignatureNoParams() {
+        doTest("A java.util.List<java.lang.String> foo()",
+                "A", "foo", "()Ljava/util/List;", RETURN_TYPE, "()Ljava/util/List<Ljava/lang/String;>;")
+    }
+
+    fun testGenericSignatureNonGenericParams() {
+        doTest("A java.util.Map.Entry<java.lang.Integer, java.lang.String> foo(int, a.b.C)",
+                "A", "foo", "(ILa/b/C;)Ljava/util/Map\$Entry;", RETURN_TYPE,
+                "(ILa/b/C;)Ljava/util/Map\$Entry<Ljava/lang/Integer;Ljava/lang/String;>;")
+    }
+
+    fun testGenericSignatureGenericParams() {
+        doTest("A java.util.Map.Entry<java.lang.Integer, java.util.Map.Entry<java.lang.Integer, java.lang.String>> " +
+                "foo(" +
+                "java.util.List<java.util.List<java.lang.String>>, " +
+                "java.util.List<java.util.Map.Entry<java.lang.Integer, java.lang.String>>)",
+                "A", "foo", "(Ljava/util/List;Ljava/util/List;)Ljava/util/Map\$Entry;", RETURN_TYPE,
+                "(Ljava/util/List<Ljava/util/List<Ljava/lang/String;>;>;" +
+                "Ljava/util/List<Ljava/util/Map\$Entry<Ljava/lang/Integer;Ljava/lang/String;>;>;)" +
+                "Ljava/util/Map\$Entry<Ljava/lang/Integer;Ljava/util/Map\$Entry<Ljava/lang/Integer;Ljava/lang/String;>;>;")
+    }
+
+    fun foo(i: List<List<String>>, x: List<Map.Entry<Int, String>>): Map.Entry<Int, Map.Entry<Int, String>> {
+        throw Exception()
+    }
+
+}
+
+data class MockTypePosition(
+        override val method: Method,
+        override val positionWithinMethod: PositionWithinMethod
+) : TypePosition
