0b6830ae78ea4c05705d26b967da502357a96556
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-29 03:26:27 +0400

    Indexing for easier annotation key lookup

--- src/org/jetbrains/kannotator/index/FileBasedDeclarationIndex.kt
+++ src/org/jetbrains/kannotator/index/FileBasedDeclarationIndex.kt
@@ -4,7 +4,7 @@ import java.io.File
 import java.util.HashMap
 import java.util.HashSet
 import org.jetbrains.kannotator.annotations.io.toAnnotationKey
-import org.jetbrains.kannotator.declarations.ClassName
+import org.jetbrains.kannotator.declarations.*
 import org.jetbrains.kannotator.declarations.Method
 import org.jetbrains.kannotator.declarations.Positions
 import org.jetbrains.kannotator.declarations.getArgumentTypes
@@ -25,13 +25,21 @@ import java.util.jar.JarEntry
 import kotlinlib.removeSuffix
 import org.jetbrains.kannotator.declarations.toFullString
 import org.jetbrains.kannotator.asm.util.forEachMethod
+import org.jetbrains.kannotator.annotations.io.parseAnnotationKey
 
 trait ClassSource {
     fun forEach(body: (ClassReader) -> Unit)
 }
 
 class DeclarationIndexImpl(classSource: ClassSource): DeclarationIndex {
-    private val methodsByClass = HashMap<ClassName, MutableMap<MethodId, Method>>();
+    private data class ClassData(
+            val className: ClassName,
+            val methodsById: Map<MethodId, Method>,
+            val methodsByName: Map<String, Collection<Method>>
+    )
+
+    private val classes = HashMap<ClassName, ClassData>()
+    private val classesByCanonicalName = HashMap<String, MutableCollection<ClassData>>();
 
     { init(classSource) }
 
@@ -39,19 +47,25 @@ class DeclarationIndexImpl(classSource: ClassSource): DeclarationIndex {
         classSource forEach {
             reader ->
             val className = ClassName.fromInternalName(reader.getClassName())
-            val idToMethod = HashMap<MethodId, Method>()
-            assert (methodsByClass[className] == null) { "Class already visited: $className" }
-            methodsByClass[className] = idToMethod
+
+            val methodsById = HashMap<MethodId, Method>()
+            val methodsByName = HashMap<String, MutableList<Method>>()
+            assert (classes[className] == null) { "Class already visited: $className" }
             reader.forEachMethod {
                 owner, access, name, desc, signature ->
                 val method = Method(className, access, name, desc, signature)
-                idToMethod[method.id] = method
+                methodsById[method.id] = method
+                methodsByName.getOrPut(name, {ArrayList()}).add(method)
             }
+
+            val classData = ClassData(className, methodsById, methodsByName)
+            classes[className] = classData
+            classesByCanonicalName.getOrPut(className.canonicalName, {HashSet()}).add(classData)
         }
     }
 
     override fun findMethod(owner: ClassName, name: String, desc: String): Method? {
-        return methodsByClass[owner]?.get(MethodId(name, desc))
+        return classes[owner]?.methodsById?.get(MethodId(name, desc))
     }
 }
 
