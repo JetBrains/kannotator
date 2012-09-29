d4edf0493a3314360d65ab0b8965cae3c62a97be
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-09-29 09:25:02 +0400

    Nullability annotation loader

---
+++ src/org/jetbrains/kannotator/main/inferNullability.kt
@@ -0,0 +1,59 @@
+package org.jetbrains.kannotator.main
+
+import java.io.File
+import org.jetbrains.kannotator.index.FileBasedClassSource
+import org.jetbrains.kannotator.index.DeclarationIndexImpl
+import org.jetbrains.kannotator.annotations.io.parseAnnotations
+import java.io.FileReader
+import org.jetbrains.kannotator.declarations.AnnotationsImpl
+import org.jetbrains.kannotator.nullability.NullabilityAnnotation
+import org.jetbrains.kannotator.index.AnnotationKeyIndex
+import org.jetbrains.kannotator.declarations.Annotations
+
+fun inferNullabilityAnnotations(jarOrClassFiles: Collection<File>, existingAnnotationFiles: Collection<File>) {
+    val source = FileBasedClassSource(jarOrClassFiles)
+    val declarationIndex = DeclarationIndexImpl(source)
+
+    val existingNullabilityAnnotations = loadNullabilityAnnotations(existingAnnotationFiles, declarationIndex)
+
+    // build method graph
+
+    // build hierarchy
+}
+
+fun loadNullabilityAnnotations(
+        annotationFiles: Collection<File>,
+        keyIndex: AnnotationKeyIndex): Annotations<NullabilityAnnotation>
+{
+    val nullabilityAnnotations = AnnotationsImpl<NullabilityAnnotation>()
+    val nullabilityAnnotationsMap = hashMap(
+            "org.jetbrains.annotations.NotNull" to NullabilityAnnotation.NOT_NULL,
+            "org.jetbrains.annotations.Nullable" to NullabilityAnnotation.NULLABLE
+    )
+
+    for (annotationFile in annotationFiles) {
+        FileReader(annotationFile) use {
+            parseAnnotations(it, {
+                key, annotations ->
+                val position = keyIndex.findPositionByAnnotationKeyString(key)
+                if (position == null) {
+                    error("Position not found for $key")
+                }
+                else {
+                    for (data in annotations) {
+                        val annotation = nullabilityAnnotationsMap[data.annotationClassFqn]
+                        if (annotation != null) {
+                            nullabilityAnnotations[position] = annotation
+                        }
+                    }
+                }
+            }, {error(it)})
+        }
+    }
+
+    return nullabilityAnnotations
+}
+
+fun error(message: String) {
+    System.err.println(message)
+}
