d0c760055274548e8c1ac59bec3d3ba9727b2174
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-10-01 18:58:30 +0400

    write annotations to file

---
+++ src/org/jetbrains/kannotator/annotations/io/annotationWriter.kt
@@ -0,0 +1,121 @@
+package org.jetbrains.kannotator.annotations.io
+
+import java.io.Writer
+import org.jetbrains.kannotator.declarations.Annotations
+import kotlinlib.println
+import java.io.File
+import org.jetbrains.kannotator.nullability.NullabilityAnnotation
+import java.util.ArrayList
+import org.jetbrains.kannotator.declarations.AnnotationsImpl
+import org.jetbrains.kannotator.declarations.TypePosition
+import java.util.HashMap
+import org.jetbrains.kannotator.declarations.Method
+import org.jetbrains.kannotator.declarations.ClassName
+import annotations.io.MockTypePosition
+import java.util.LinkedHashMap
+import kotlinlib.buildString
+
+
+fun writeAnnotations<A>(writer: Writer, annotations: Collection<Annotations<A>>, renderer: (A) -> AnnotationData) {
+    val sb = StringBuilder()
+    val printer = XmlPrinter(sb)
+    printer.openTag("root")
+    printer.pushIndent()
+    for (annotation in annotations) {
+        annotation.forEach {
+            typePosition, annotation ->
+            printer.openTag("item", hashMap(Pair("name", typePosition.toAnnotationKey())))
+            printer.pushIndent()
+            val annotationData = renderer(annotation)
+            if (annotationData.attributes.size() < 1) {
+                printer.openTag("annotation", hashMap(Pair("name", annotationData.annotationClassFqn)), true)
+            } else {
+                printer.openTag("annotation", hashMap(Pair("name", annotationData.annotationClassFqn)))
+                for ((name, value) in annotationData.attributes) {
+                    val attributesMap = LinkedHashMap<String, String>()
+                    attributesMap.put("name", name)
+                    attributesMap.put("val", value)
+                    printer.pushIndent()
+                    printer.openTag("val", attributesMap, true, '"')
+                    printer.popIndent()
+                }
+                printer.closeTag("annotation")
+            }
+            printer.popIndent()
+            printer.closeTag("item")
+
+        }
+    }
+    printer.popIndent()
+    printer.closeTag("root")
+
+    writer.write(sb.toString())
+    writer.close()
+
+}
+
+class XmlPrinter(val sb: StringBuilder) {
+    private val INDENTATION_UNIT = "    ";
+    private var indent = "";
+
+    public fun println() {
+        sb.println()
+    }
+
+    fun openTag(tagName: String, attributes: Map<String, String>? = null, isClosed: Boolean = false, quoteChar : Char = '\'') {
+        sb.append(indent)
+        sb.append("<").append(tagName)
+        if (attributes != null) {
+            for ((name, value) in attributes) {
+                sb.append(" ").append(escape(name)).append("=").append(quoteChar).append(escape(value)).append(quoteChar)
+            }
+        }
+        if (isClosed) {
+            sb.append("/>")
+        }
+        else {
+            sb.append(">")
+        }
+        println()
+    }
+
+    fun closeTag(tagName: String) {
+        sb.append(indent);
+        sb.append("</").append(tagName).append(">")
+        println()
+    }
+
+    public fun pushIndent() {
+        indent += INDENTATION_UNIT;
+    }
+
+    public fun popIndent() {
+        if (indent.length() < INDENTATION_UNIT.length()) {
+            throw IllegalStateException("No indentation to pop");
+        }
+
+        indent = indent.substring(INDENTATION_UNIT.length());
+    }
+}
+
+private fun escape(str: String): String {
+    return buildString {
+        sb ->
+        for (c in str) {
+            when {
+                c == '<' -> sb.append("&lt;")
+                c == '>' -> sb.append("&gt;")
+                c == '\"' || c == '\'' -> {
+                    sb.append("&quot;")
+                }
+                else -> sb.append(c);
+            }
+        }
+    }
+}
+
+
+
+
+
+
---
+++ test/annotations/io/WriteAnnotationTest.kt
@@ -0,0 +1,125 @@
+package annotations.io
+
+import java.io.File
+import java.io.FileWriter
+import java.util.HashMap
+import java.util.LinkedHashSet
+import java.util.LinkedList
+import junit.framework.Assert
+import kotlinlib.recurseFiltered
+import org.jetbrains.kannotator.annotations.io.AnnotationData
+import org.jetbrains.kannotator.annotations.io.parseAnnotations
+import org.jetbrains.kannotator.annotations.io.toAnnotationKey
+import org.jetbrains.kannotator.annotations.io.writeAnnotations
+import org.jetbrains.kannotator.declarations.Annotations
+import org.jetbrains.kannotator.declarations.ClassName
+import org.jetbrains.kannotator.declarations.Method
+import org.jetbrains.kannotator.declarations.Positions
+import org.jetbrains.kannotator.declarations.TypePosition
+import org.jetbrains.kannotator.declarations.getArgumentTypes
+import org.jetbrains.kannotator.declarations.internalNameToCanonical
+import org.jetbrains.kannotator.declarations.isStatic
+import org.jetbrains.kannotator.util.processJar
+import org.junit.Test
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+
+public class WriteAnnotationTest {
+
+    Test fun testAll() {
+        doTest(array(File("lib")),
+                array(File("testData/annotations/write")))
+    }
+
+    /*Test fun testJdk() {
+        doTest(array(File("c:/Program Files/Java/jdk1.6.0_30/jre/lib/rt.jar")),
+                array(File("testData/annotations/jdk-annotations")))
+    }*/
+
+    fun doTest(jarDirsOrFiles: Array<File>, annotationDirsOrFiles: Array<File>) {
+        val classToReaderMap = HashMap<String, ClassReader>()
+        for (dirOrFile in jarDirsOrFiles) {
+            dirOrFile.recurseFiltered({ it.extension == "jar" }) {
+                file ->
+                visitAllInJar(file, {
+                    className, classReader ->
+                    classToReaderMap.put(className, classReader)
+                })
+            }
+        }
+
+        for (dirOrFile in annotationDirsOrFiles) {
+            dirOrFile.recurseFiltered({ it.extension == "xml" }) {
+                file ->
+                println("Processing ${file.getAbsolutePath()}")
+                val typePositionAndAnnotationData = LinkedHashSet<Pair<TypePosition, LinkedList<AnnotationData>>>()
+                parseAnnotations(file.reader(), { key, annotationData ->
+                    val classReader = classToReaderMap.get(key.substring(0, key.indexOf(" ")))
+                    if (classReader != null) {
+                        getTypePosition(classReader) {
+                            typePosition ->
+                            if (typePosition.toAnnotationKey() == key) {
+                                typePositionAndAnnotationData.add(Pair(typePosition, annotationData.toLinkedList()))
+                            }
+                        }
+                    }
+                    else {
+                        println("Cannot find class reader for ${key.substring(0, key.indexOf(" "))} class")
+                    }
+                }, { str -> println(str) })
+
+                val annotationsList = LinkedList<Annotations<AnnotationData>>()
+                annotationsList.add(AnnotationsWithAnnotationData(typePositionAndAnnotationData))
+
+                val actualFile = File.createTempFile("writeAnnotations", file.getName())
+                actualFile.createNewFile()
+                println("Saved file: ${actualFile.getAbsolutePath()}")
+                writeAnnotations<AnnotationData>(FileWriter(actualFile), annotationsList, {
+                    annotation -> annotation
+                })
+                Assert.assertEquals(file.readText().trim(), actualFile.readText().trim())
+            }
+        }
+    }
+
+    fun visitAllInJar(file: File, handler: (String, ClassReader) -> Unit) {
+        processJar(file) {
+            file, owner, reader ->
+            handler(reader.getClassName().internalNameToCanonical(), reader)
+        }
+    }
+
+    fun getTypePosition(reader: ClassReader, handler: (TypePosition) -> Unit) {
+        reader.accept(object : ClassVisitor(Opcodes.ASM4) {
+            override fun visitMethod(access: Int, name: String, desc: String, signature: String?, exceptions: Array<out String>?): MethodVisitor? {
+                val method = Method(ClassName.fromInternalName(reader.getClassName()), access, name, desc, signature)
+                val positions = Positions(method)
+                val skip = if (method.isStatic()) 0 else 1
+                for (i in skip..method.getArgumentTypes().size) {
+                    handler(positions.forParameter(i).position)
+                }
+                handler(positions.forReturnType().position)
+                return null
+            }
+        }, 0)
+    }
+}
+
+private class AnnotationsWithAnnotationData(val data: Set<Pair<TypePosition, LinkedList<AnnotationData>>>): Annotations<AnnotationData> {
+    override fun forEach(body: (TypePosition, AnnotationData) -> Unit) {
+        for ((position, annotations) in data) {
+            for (annotation in annotations) {
+                body(position, annotation)
+            }
+        }
+    }
+
+    override fun get(typePosition: TypePosition): AnnotationData? {
+        throw UnsupportedOperationException()
+    }
+}
+
+
+
