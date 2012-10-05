3dfd503077ed6781100bee99177799fd4a1f993e
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-10-05 09:04:14 -0700

    Preserve order when converting to a map

--- kotlinlib/kotlinlib/pairsToMap.kt
+++ kotlinlib/kotlinlib/pairsToMap.kt
@@ -1,9 +1,9 @@
 package kotlinlib
 
-import java.util.HashMap
+import java.util.LinkedHashMap
 
 public fun <K, V> Iterator<Pair<K, V>>.toMap(): Map<K, V> {
-    val map = HashMap<K, V>()
+    val map = LinkedHashMap<K, V>()
     for ((k, v) in this) {
         map[k] = v
     }
