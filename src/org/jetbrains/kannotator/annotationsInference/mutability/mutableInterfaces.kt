95450b68c97cfe3b764d792933445709abfdcc5c
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2012-10-09 16:21:09 +0400

    changed format of mutable interfaces to asm internal

--- src/org/jetbrains/kannotator/annotationsInference/mutability/mutableInterfaces.kt
+++ src/org/jetbrains/kannotator/annotationsInference/mutability/mutableInterfaces.kt
@@ -3,19 +3,19 @@ package org.jetbrains.kannotator.annotationsInference.mutability
 import org.objectweb.asm.tree.MethodInsnNode
 
 val mutableInterfaces: Map<String, List<String>> = hashMap(
-        "java.util.Collection" to arrayList("add", "remove", "addAll", "removeAll", "retainAll", "clear"),
-        "java.util.List" to arrayList("add", "remove", "addAll", "removeAll", "retainAll", "clear", "set", "add", "remove"),
-        "java.util.Set" to arrayList("add", "remove", "AddAll", "removeAll", "retainAll", "clear"),
-        "java.util.Map" to arrayList("put", "remove", "putAll", "clear"),
-        "java.util.Map\$Entry" to arrayList("setValue"),
-        "java.util.Iterator" to arrayList("remove")
+        "java/util/Collection" to arrayList("add", "remove", "addAll", "removeAll", "retainAll", "clear"),
+        "java/util/List" to arrayList("add", "remove", "addAll", "removeAll", "retainAll", "clear", "set", "add", "remove"),
+        "java/util/Set" to arrayList("add", "remove", "AddAll", "removeAll", "retainAll", "clear"),
+        "java/util/Map" to arrayList("put", "remove", "putAll", "clear"),
+        "java/util/Map\$Entry" to arrayList("setValue"),
+        "java/util/Iterator" to arrayList("remove")
 )
 
 val propagatingMutability: Map<String, List<String>> = hashMap(
-        "java.util.Collection" to arrayList("iterator"),
-        "java.util.List" to arrayList("iterator", "listIterator"),
-        "java.util.Set" to arrayList("iterator"),
-        "java.util.Map" to arrayList("keySet", "values", "entrySet")
+        "java/util/Collection" to arrayList("iterator"),
+        "java/util/List" to arrayList("iterator", "listIterator"),
+        "java/util/Set" to arrayList("iterator"),
+        "java/util/Map" to arrayList("keySet", "values", "entrySet")
 )
 
 fun isInvocationRequiredMutability(instruction: MethodInsnNode) : Boolean =
@@ -25,7 +25,7 @@ fun isPropagatingMutability(instruction: MethodInsnNode) : Boolean =
         propagatingMutability.containsInvocation(instruction)
 
 private fun Map<String, List<String>>.containsInvocation(instruction: MethodInsnNode) : Boolean {
-    val className = instruction.owner!!.replace("/", ".")
+    val className = instruction.owner!!
     val methodName = instruction.name!!
     return this[className]?.contains(methodName) ?: false
 }
