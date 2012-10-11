459705073e7ef7eb96dd230afbe725c1773de2ec
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-10-11 18:26:25 +0400

    Testing writing field annotations

--- test/annotations/io/WriteAnnotationTest.kt
+++ test/annotations/io/WriteAnnotationTest.kt
@@ -6,31 +6,33 @@ import java.util.HashMap
 import java.util.LinkedHashSet
 import java.util.LinkedList
 import junit.framework.Assert
+import kotlinlib.prefixUpTo
 import kotlinlib.recurseFiltered
 import org.jetbrains.kannotator.annotations.io.AnnotationData
 import org.jetbrains.kannotator.annotations.io.parseAnnotations
 import org.jetbrains.kannotator.annotations.io.toAnnotationKey
 import org.jetbrains.kannotator.annotations.io.writeAnnotations
+import org.jetbrains.kannotator.declarations.AnnotationPosition
 import org.jetbrains.kannotator.declarations.Annotations
 import org.jetbrains.kannotator.declarations.ClassName
+import org.jetbrains.kannotator.declarations.Field
 import org.jetbrains.kannotator.declarations.Method
 import org.jetbrains.kannotator.declarations.PositionsWithinMember
-import org.jetbrains.kannotator.declarations.AnnotationPosition
-import org.jetbrains.kannotator.declarations.getArgumentTypes
+import org.jetbrains.kannotator.declarations.forEachValidPosition
+import org.jetbrains.kannotator.declarations.getFieldAnnotatedType
 import org.jetbrains.kannotator.declarations.internalNameToCanonical
-import org.jetbrains.kannotator.declarations.isStatic
 import org.jetbrains.kannotator.util.processJar
 import org.junit.Test
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.FieldVisitor
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class WriteAnnotationTest {
 
     Test fun testAll() {
-        doTest(array(File("lib")),
-                array(File("testData/annotations/write")))
+        doTest(array(File("lib")), array(File("testData/annotations/write")))
     }
 
     /*Test fun testJdk() {
@@ -56,17 +58,16 @@ public class WriteAnnotationTest {
                 println("Processing ${file.getAbsolutePath()}")
                 val typePositionAndAnnotationData = LinkedHashSet<Pair<AnnotationPosition, LinkedList<AnnotationData>>>()
                 parseAnnotations(file.reader(), { key, annotationData ->
-                    val classReader = classToReaderMap.get(key.substring(0, key.indexOf(" ")))
+                    val classReader = classToReaderMap.get(key.prefixUpTo(' '))
                     if (classReader != null) {
-                        getTypePosition(classReader) {
-                            typePosition ->
-                            if (typePosition.toAnnotationKey() == key) {
-                                typePositionAndAnnotationData.add(Pair(typePosition, annotationData.toLinkedList()))
+                        forAllClassAnnotationPositions(classReader) { annotationPosition ->
+                            if (annotationPosition.toAnnotationKey() == key) {
+                                typePositionAndAnnotationData.add(Pair(annotationPosition, annotationData.toLinkedList()))
                             }
                         }
                     }
                     else {
-                        println("Cannot find class reader for ${key.substring(0, key.indexOf(" "))} class")
+                        println("Cannot find class reader for ${key.prefixUpTo(' ')} class")
                     }
                 }, { str -> println(str) })
 
@@ -91,16 +92,18 @@ public class WriteAnnotationTest {
         }
     }
 
-    fun getTypePosition(reader: ClassReader, handler: (AnnotationPosition) -> Unit) {
-        reader.accept(object : ClassVisitor(Opcodes.ASM4) {
+    private fun forAllClassAnnotationPositions(classReader: ClassReader, handler: (AnnotationPosition) -> Unit) {
+        classReader.accept(object : ClassVisitor(Opcodes.ASM4) {
             override fun visitMethod(access: Int, name: String, desc: String, signature: String?, exceptions: Array<out String>?): MethodVisitor? {
-                val method = Method(ClassName.fromInternalName(reader.getClassName()), access, name, desc, signature)
+                val method = Method(ClassName.fromInternalName(classReader.getClassName()), access, name, desc, signature)
                 val positions = PositionsWithinMember(method)
-                val skip = if (method.isStatic()) 0 else 1
-                for (i in skip..method.getArgumentTypes().size) {
-                    handler(positions.forParameter(i).position)
-                }
-                handler(positions.forReturnType().position)
+                positions.forEachValidPosition(handler)
+                return null
+            }
+
+            public override fun visitField(access: Int, name: String, desc: String, signature: String?, value: Any?): FieldVisitor? {
+                val field = Field(ClassName.fromInternalName(classReader.getClassName()), access, name, desc, signature, value)
+                handler(getFieldAnnotatedType(field).position)
                 return null
             }
         }, 0)
