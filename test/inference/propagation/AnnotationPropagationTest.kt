b86227530b92ab8423d687d2925316fd75df197d
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-10-13 23:05:24 +0400

    doPropagationTest extracted

--- test/inference/propagation/AnnotationPropagationTest.kt
+++ test/inference/propagation/AnnotationPropagationTest.kt
@@ -1,60 +1,19 @@
 package inference.propagation
 
-import inferenceData.propagation.Conflicts
-import inferenceData.propagation.DiamondHierarchy
-import inferenceData.propagation.LinearHierarchy
-import inferenceData.propagation.YHierarchy
-import java.util.LinkedHashSet
-import junit.framework.Assert.*
+import inferenceData.propagation.*
 import junit.framework.TestCase
-import kotlinlib.*
-import org.jetbrains.kannotator.annotations.io.getAnnotationsFromClassFiles
-import org.jetbrains.kannotator.annotations.io.toAnnotationKey
-import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation
-import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation.*
 import org.jetbrains.kannotator.annotationsInference.nullability.NullabiltyLattice
-import org.jetbrains.kannotator.annotationsInference.nullability.classNamesToNullabilityAnnotation
 import org.jetbrains.kannotator.annotationsInference.propagation.propagateMetadata
-import org.jetbrains.kannotator.classHierarchy.buildClassHierarchyGraph
-import org.jetbrains.kannotator.classHierarchy.buildMethodHierarchy
-import org.jetbrains.kannotator.declarations.AnnotationPosition
-import org.jetbrains.kannotator.declarations.Annotations
 import org.jetbrains.kannotator.index.ClassSource
 import util.Classes
-import inferenceData.propagation.*
 
 class AnnotationPropagationTest : TestCase() {
 
     fun doTest(classSource: ClassSource) {
-        val classHierarchy = buildClassHierarchyGraph(classSource)
-        val methodHierarchy = buildMethodHierarchy(classHierarchy)
-
-        val initialAnnotations = getAnnotationsFromClassFiles(classSource) { names -> classNamesToNullabilityAnnotation(names) }
-
-        val propagatedAnnotations = propagateMetadata(methodHierarchy, NullabiltyLattice, initialAnnotations)
-
-        val EXPECT_NN = "inferenceData.annotations.ExpectNotNull"
-        val EXPECT_N = "inferenceData.annotations.ExpectNullable"
-        val NN = "org.jetbrains.annotations.NotNull"
-        val N = "org.jetbrains.annotations.Nullable"
-        val expectedAnnotationClasses = hashMap(
-                NN to NOT_NULL,
-                N to NULLABLE
-        )
-        val expectedAnnotations = getAnnotationsFromClassFiles(classSource) {
-            names ->
-                when {
-                    names.isEmpty() -> null
-                    EXPECT_N in names -> NULLABLE
-                    EXPECT_NN in names -> NOT_NULL
-                    else -> {
-                        assert (names.size == 1) {"Multiple annotations but no Expect* ones: $names"}
-                        expectedAnnotationClasses[names.first()]
-                    }
-                }
+        doPropagationTest(classSource) {
+            methodHierarchy, initialAnnotations ->
+            propagateMetadata(methodHierarchy, NullabiltyLattice, initialAnnotations)
         }
-
-        assertEquals(expectedAnnotations.toDeclarations(), propagatedAnnotations.toDeclarations())
     }
 
     fun testConflicts() {
@@ -131,19 +90,3 @@ class AnnotationPropagationTest : TestCase() {
         ))
     }
 }
-
-fun Annotations<NullabilityAnnotation>.toDeclarations(): String {
-    val positions = LinkedHashSet<AnnotationPosition>()
-    forEach {
-        pos, ann ->
-        positions.add(pos)
-    }
-    val s = buildString {
-        sb ->
-        for (pos in positions.toSortedList { a, b -> a.toAnnotationKey().compareTo(b.toAnnotationKey()) }) {
-            sb.append("${pos.toAnnotationKey()}\n  ${this[pos]}\n\n")
-        }
-    }.toSystemLineSeparators()
-    return s
-}
-
---
+++ test/inference/propagation/propagationTestUtil.kt
@@ -0,0 +1,68 @@
+package inference.propagation
+
+import junit.framework.Assert.*
+import org.jetbrains.kannotator.annotations.io.getAnnotationsFromClassFiles
+import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation
+import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation.*
+import org.jetbrains.kannotator.annotationsInference.nullability.classNamesToNullabilityAnnotation
+import org.jetbrains.kannotator.classHierarchy.HierarchyGraph
+import org.jetbrains.kannotator.classHierarchy.buildClassHierarchyGraph
+import org.jetbrains.kannotator.classHierarchy.buildMethodHierarchy
+import org.jetbrains.kannotator.declarations.Annotations
+import org.jetbrains.kannotator.declarations.Method
+import org.jetbrains.kannotator.index.ClassSource
+import java.util.LinkedHashSet
+import org.jetbrains.kannotator.declarations.AnnotationPosition
+import kotlinlib.*
+import org.jetbrains.kannotator.annotations.io.toAnnotationKey
+
+fun doPropagationTest(
+        classSource: ClassSource,
+        propagate: (HierarchyGraph<Method>, Annotations<NullabilityAnnotation>) -> Annotations<NullabilityAnnotation>
+) {
+    val classHierarchy = buildClassHierarchyGraph(classSource)
+    val methodHierarchy = buildMethodHierarchy(classHierarchy)
+
+    val initialAnnotations = getAnnotationsFromClassFiles(classSource) { names -> classNamesToNullabilityAnnotation(names) }
+
+    val propagatedAnnotations = propagate(methodHierarchy, initialAnnotations)
+
+    val EXPECT_NN = "inferenceData.annotations.ExpectNotNull"
+    val EXPECT_N = "inferenceData.annotations.ExpectNullable"
+    val NN = "org.jetbrains.annotations.NotNull"
+    val N = "org.jetbrains.annotations.Nullable"
+    val expectedAnnotationClasses = hashMap(
+            NN to NOT_NULL,
+            N to NULLABLE
+    )
+    val expectedAnnotations = getAnnotationsFromClassFiles(classSource) {
+        names ->
+        when {
+            names.isEmpty() -> null
+            EXPECT_N in names -> NULLABLE
+            EXPECT_NN in names -> NOT_NULL
+            else -> {
+                assert (names.size == 1) {"Multiple annotations but no Expect* ones: $names"}
+                expectedAnnotationClasses[names.first()]
+            }
+        }
+    }
+
+    assertEquals(expectedAnnotations.toDeclarations(), propagatedAnnotations.toDeclarations())
+}
+
+
+fun Annotations<NullabilityAnnotation>.toDeclarations(): String {
+    val positions = LinkedHashSet<AnnotationPosition>()
+    forEach {
+        pos, ann ->
+        positions.add(pos)
+    }
+    val s = buildString {
+        sb ->
+        for (pos in positions.toSortedList { a, b -> a.toAnnotationKey().compareTo(b.toAnnotationKey()) }) {
+            sb.append("${pos.toAnnotationKey()}\n  ${this[pos]}\n\n")
+        }
+    }.toSystemLineSeparators()
+    return s
+}
