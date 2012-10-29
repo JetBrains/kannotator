1a420a3d91e603fb1a3eed2b18438bb399a7df16
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-10-29 18:22:27 +0400

    Index fields

--- test/util/ClassPathDeclarationIndex.kt
+++ test/util/ClassPathDeclarationIndex.kt
@@ -11,21 +11,29 @@ import org.jetbrains.kannotator.declarations.Field
 import java.util.HashSet
 
 object ClassPathDeclarationIndex : DeclarationIndex {
+    private val FIELD_DEFAULT_DESCRIPTOR = ""
+
     data class SearchQuery(val owner : ClassName, val name: String, val desc: String)
 
-    private val cache = HashMap<SearchQuery, Method>()
+    private val cacheMethods = HashMap<SearchQuery, Method>()
+    private val cacheFields = HashMap<SearchQuery, Field>()
 
     override fun findMethod(owner: ClassName, name: String, desc: String) : Method? {
         val query = SearchQuery(owner, name, desc)
-        if (!cache.containsKey(query)) {
+        if (!cacheMethods.containsKey(query)) {
             search(query)
         }
 
-        return cache[query]
+        return cacheMethods[query]
     }
 
     override fun findField(owner: ClassName, name: String): Field? {
-        throw UnsupportedOperationException()
+        val query = SearchQuery(owner, name, FIELD_DEFAULT_DESCRIPTOR)
+        if (!cacheFields.containsKey(query)) {
+            search(query)
+        }
+
+        return cacheFields[query]
     }
 
     private fun search(query : SearchQuery) {
@@ -38,15 +46,22 @@ object ClassPathDeclarationIndex : DeclarationIndex {
         val node = ClassNode()
         reader.accept(node, SKIP_CODE or SKIP_DEBUG or SKIP_FRAMES)
 
-        val foundPairs = node.methods.map {
+        val className = ClassName.fromInternalName(node.name)
+
+        node.methods.forEach {
             val methodNode = it
-            val className = ClassName.fromInternalName(node.name)
             val methodQuery = SearchQuery(className, methodNode.name, methodNode.desc)
             val method = Method(className, methodNode.access, methodNode.name, methodNode.desc, methodNode.signature)
 
-            Pair(methodQuery, method)
+            cacheMethods[methodQuery] = method
         }
 
-        cache.putAll(*foundPairs.toArray(array()))
+        node.fields.forEach {
+            val fieldNode = it
+            val fieldQuery = SearchQuery(className, fieldNode.name, FIELD_DEFAULT_DESCRIPTOR)
+            val field = Field(className, fieldNode.access, fieldNode.name, fieldNode.desc, fieldNode.signature, fieldNode.value)
+
+            cacheFields[fieldQuery] = field
+        }
     }
 }
