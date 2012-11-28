815a53d6361dc0124ba94ffc4a5d2978612efbe8
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-11-28 16:19:50 +0400

    First version of the infer action dialog

--- JarAnnotator/src/org/jetbrains/annotator/actions/AnnotateJarAction.kt
+++ JarAnnotator/src/org/jetbrains/annotator/actions/AnnotateJarAction.kt
@@ -2,12 +2,23 @@ package org.jetbrains.annotator.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.diagnostic.Log
-import org.jetbrains.kannotator.main.NullabilityInferrer
+import org.jetbrains.kannotator.index.ClassSource
+import org.jetbrains.kannotator.main.AnnotationInferrer
+import org.jetbrains.kannotator.main.inferAnnotations
+import org.objectweb.asm.ClassReader
 
 public class AnnotateJarAction: AnAction() {
     public override fun actionPerformed(e: AnActionEvent?) {
-        val inferrer = NullabilityInferrer()
-        Log.print("Annotating jar $inferrer")
+        val dlg = InferAnnotationDialog(e?.getProject())
+        if (dlg.showAndGet()) {
+            val fakeClassSource = object : ClassSource {
+                override fun forEach(body: (ClassReader) -> Unit) {
+                    println("Hello from kannotator")
+                }
+            }
+
+            inferAnnotations<String>(fakeClassSource, arrayList(), hashMap("test" to (org.jetbrains.kannotator.main.MUTABILITY_INFERRER as AnnotationInferrer<Any>)))
+            return
+        }
     }
 }
