e8437d79677eac9c608105e6c3faedd09464b6cc
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2012-11-30 18:48:55 +0400

    Add "subtract" operation for collections

--- kotlinlib/kotlinlib/collectionIntersect.kt
+++ kotlinlib/kotlinlib/collectionIntersect.kt
@@ -7,3 +7,9 @@ public fun <T> Collection<T>.intersect(other: Collection<T>): Set<T> {
     resultSet.retainAll(other)
     return resultSet
 }
+
+public fun <T> Collection<T>.subtract(other: Collection<T>): Set<T> {
+    val resultSet = LinkedHashSet(this)
+    resultSet.removeAll(other)
+    return resultSet
+}
