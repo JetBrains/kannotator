63893831eb07577729cc4cf90dfd61b2239c61a8
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2012-12-07 16:05:59 +0400

    Load annotations from logs

--- src/org/jetbrains/kannotator/annotations/io/annotationParser.kt
+++ src/org/jetbrains/kannotator/annotations/io/annotationParser.kt
@@ -6,6 +6,74 @@ import kotlinlib.buildString
 import org.xml.sax.AttributeList
 import org.xml.sax.HandlerBase
 import java.util.ArrayList
+import java.io.File
+import org.jetbrains.kannotator.index.AnnotationKeyIndex
+import java.util.Collections
+import java.io.Writer
+import java.util.LinkedHashMap
+import kotlinlib.buildString
+import kotlinlib.println
+import org.jetbrains.kannotator.declarations.AnnotationPosition
+import org.jetbrains.kannotator.index.AnnotationKeyIndex
+import java.io.File
+import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation
+import org.jetbrains.kannotator.index.DeclarationIndex
+import junit.framework.TestCase
+import junit.framework.Assert.*
+import java.io.File
+import java.util.Collections
+import java.io.PrintStream
+import java.io.FileOutputStream
+import kotlinlib.*
+import org.jetbrains.kannotator.annotations.io.toAnnotationKey
+import org.jetbrains.kannotator.main.ProgressMonitor
+import org.jetbrains.kannotator.declarations.Method
+import java.io.FileInputStream
+import interpreter.readWithBuffer
+import java.util.TreeMap
+import org.jetbrains.kannotator.annotationsInference.Annotation
+import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation
+import java.util.ArrayList
+import util.assertEqualsOrCreate
+import org.jetbrains.kannotator.declarations.AnnotationsImpl
+import org.jetbrains.kannotator.declarations.AnnotationPosition
+import kotlin.test.assertTrue
+import org.jetbrains.kannotator.declarations.Annotations
+import org.jetbrains.kannotator.main.*
+import util.findJarsInLibFolder
+import org.jetbrains.kannotator.index.FileBasedClassSource
+import org.junit.Assert
+import util.*
+import java.util.HashSet
+import org.jetbrains.kannotator.annotations.io.writeAnnotations
+import java.io.FileWriter
+import org.jetbrains.kannotator.declarations.PositionsForMethod
+import org.jetbrains.kannotator.annotations.io.AnnotationDataImpl
+import org.jetbrains.kannotator.kotlinSignatures.renderMethodSignature
+import org.jetbrains.kannotator.annotationsInference.mutability.MutabilityAnnotation
+import org.jetbrains.kannotator.kotlinSignatures.kotlinSignatureToAnnotationData
+import java.io.StringWriter
+import org.jetbrains.kannotator.index.AnnotationKeyIndex
+import org.jetbrains.kannotator.declarations.MutableAnnotations
+import org.jetbrains.kannotator.index.DeclarationIndex
+import kotlinSignatures.KotlinSignatureTestData.MutabilityNoAnnotations
+import java.io.BufferedReader
+import java.io.FileReader
+import org.jetbrains.kannotator.annotations.io.AnnotationData
+import org.jetbrains.kannotator.declarations.isPublicOrProtected
+import org.jetbrains.kannotator.declarations.isPublic
+import org.jetbrains.kannotator.annotations.io.parseAnnotations
+import java.util.HashMap
+import org.jetbrains.kannotator.declarations.forEachValidPosition
+import org.jetbrains.kannotator.annotationsInference.nullability.*
+import kotlin.dom.addClass
+import java.util.LinkedHashMap
+import org.jetbrains.kannotator.classHierarchy.*
+import org.jetbrains.kannotator.declarations.ClassMember
+import org.jetbrains.kannotator.declarations.getInternalPackageName
+import org.jetbrains.kannotator.funDependecy.buildFunctionDependencyGraph
+import org.jetbrains.kannotator.funDependecy.getTopologicallySortedStronglyConnectedComponents
+import org.jetbrains.kannotator.declarations.*
 
 trait AnnotationData {
     val annotationClassFqn: String
@@ -99,5 +167,35 @@ private fun escapeAttributes(str: String): String {
     }
 }
 
+fun loadAnnotationsFromLogs(
+        sourceFiles: Collection<File>,
+        keyIndex: AnnotationKeyIndex
+): Annotations<NullabilityAnnotation> {
+    val annotations = AnnotationsImpl<NullabilityAnnotation>()
 
+    for (sourceFile in sourceFiles) {
+        BufferedReader(FileReader(sourceFile)) use { br->
+            val it = br.lineIterator()
 
+            while (it.hasNext()) {
+                val key = it.next()
+                val value = it.next()
+
+                val pos = keyIndex.findPositionByAnnotationKeyString(key)
+                if (pos == null)
+                    continue
+
+                val annotation =
+                        if (value == "NULLABLE") NullabilityAnnotation.NULLABLE
+                        else if (value == "NOT_NULL") NullabilityAnnotation.NOT_NULL
+                        else null
+
+                if (annotation != null) {
+                    annotations[pos] = annotation
+                }
+            }
+        }
+    }
+
+    return annotations
+}
