5af1e63b0d27e745eee0d562290ac371900f1b9e
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2013-02-14 19:49:13 +0400

    Add ResultFrame class

---
+++ src/org/jetbrains/kannotator/controlFlow/builder/analysis/resultFrame.kt
@@ -0,0 +1,15 @@
+package org.jetbrains.kannotator.controlFlow.builder.analysis
+
+import org.objectweb.asm.tree.analysis.Value
+import org.objectweb.asm.tree.analysis.Frame
+import org.objectweb.asm.tree.AbstractInsnNode
+import java.util.Collections
+
+public data class ResultFrame<V: Value>(val frame: Frame<V>, val insnNode: AbstractInsnNode)
+
+fun <V: Value> singletonOrEmptyResult(frame: Frame<V>?): Collection<ResultFrame<V>> {
+    return if (frame != null)
+        Collections.singletonList(pseudoErrorResult(frame))
+    else
+        Collections.emptyList<ResultFrame<V>>()
+}
