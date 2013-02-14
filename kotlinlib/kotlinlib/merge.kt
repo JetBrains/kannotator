02def47c02059e5986b237a4c06fbc8695f0b006
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2013-02-14 19:41:14 +0400

    Add map merge to kotlinlib

---
+++ kotlinlib/kotlinlib/merge.kt
@@ -0,0 +1,22 @@
+package kotlinlib
+
+import java.util.HashMap
+
+fun <K, V, R> mapMerge(
+        m1: Map<K, V>, m2: Map<K, V>, keys: Iterable<K>,
+        merger: (key: K, v1: V, v2: V) -> R
+): Map<K, R> {
+    return mapMergeTo(m1, m2, keys, HashMap<K, R>(), merger)
+}
+
+fun <K, V, R, M: MutableMap<K, R>> mapMergeTo(
+        m1: Map<K, V>, m2: Map<K, V>, keys: Iterable<K>,
+        resultMap: M,
+        merger: (key: K, v1: V, v2: V) -> R
+): M {
+    for (key in keys) {
+        resultMap.put(key, merger(key, m1.get(key), m2.get(key)))
+    }
+
+    return resultMap
+}
