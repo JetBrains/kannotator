b9be2463055b6859f9aa10277b5571cf437d24e2
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2013-03-01 14:49:46 +0400

    Move general operations on annotations to annotations.kt

--- src/org/jetbrains/kannotator/annotations/annotations.kt
+++ src/org/jetbrains/kannotator/annotations/annotations.kt
@@ -8,3 +8,20 @@ trait Annotations<out A: Any> {
 trait MutableAnnotations<A> : Annotations<A> {
     fun set(typePosition: AnnotationPosition, annotation: A)
 }
+
+public fun <A: Any> MutableAnnotations<A>.setIfNotNull(position: AnnotationPosition, annotation: A?) {
+    if (annotation != null) {
+        this[position] = annotation
+    }
+}
+
+public fun <A: Any> MutableAnnotations<A>.copyAllChanged(
+        annotations: Annotations<A>,
+        merger: (pos: AnnotationPosition, previous: A?, new: A) -> A = { pos, previous, new -> new }) {
+    annotations.forEach { pos, ann ->
+        val previous = this[pos]
+        if (previous != ann) {
+            this[pos] = merger(pos, previous, ann)
+        }
+    }
+}
--- src/org/jetbrains/kannotator/annotations/annotationsImpl.kt
+++ src/org/jetbrains/kannotator/annotations/annotationsImpl.kt
@@ -24,20 +24,3 @@ class AnnotationsImpl<A: Any>(val delegate: Annotations<A>? = null) : MutableAnn
         data[typePosition] = annotation
     }
 }
-
-public fun <A: Any> MutableAnnotations<A>.setIfNotNull(position: AnnotationPosition, annotation: A?) {
-    if (annotation != null) {
-        this[position] = annotation
-    }
-}
-
-public fun <A: Any> MutableAnnotations<A>.copyAllChanged(
-        annotations: Annotations<A>,
-        merger: (pos: AnnotationPosition, previous: A?, new: A) -> A = { pos, previous, new -> new }) {
-    annotations.forEach { pos, ann ->
-        val previous = this[pos]
-        if (previous != ann) {
-            this[pos] = merger(pos, previous, ann)
-        }
-    }
-}
