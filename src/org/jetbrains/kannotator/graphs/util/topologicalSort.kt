6a2dcba3ad1dcd6e6d043423eedd13ffc5ef4531
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2013-03-04 16:09:45 +0400

    Generify topological sorting

--- src/org/jetbrains/kannotator/graphs/util/topologicalSort.kt
+++ src/org/jetbrains/kannotator/graphs/util/topologicalSort.kt
@@ -4,9 +4,11 @@ import funDependency.SCCFinder
 import java.util.LinkedList
 import java.util.HashSet
 import kotlinlib.topologicallySort
+import org.jetbrains.kannotator.graphs.Graph
+import org.jetbrains.kannotator.graphs.Node
 
-fun <A> DependencyGraph<A>.getTopologicallySortedStronglyConnectedComponents(): List<Set<DependencyNode<A>>> {
-    val sccFinder = SCCFinder(this, { g -> g.allNodes }, { m -> m.outgoingEdges.map{ it.to } })
+fun <A, L> Graph<A, L>.getTopologicallySortedStronglyConnectedComponents(): List<Set<Node<A, L>>> {
+    val sccFinder = SCCFinder(this, { g -> g.nodes }, { m -> m.outgoingEdges.map{ it.to } })
     val components = sccFinder.getAllComponents()
 
     return components.topologicallySort {
