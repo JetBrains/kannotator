8f7f394ecab5400ff24b8d321f6fa59718b66a3f
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2013-03-15 15:52:14 +0400

    Change @Propagated data to enum

--- kannotatorRuntime/src/org/jetbrains/kannotator/runtime/annotations/Propagated.java
+++ kannotatorRuntime/src/org/jetbrains/kannotator/runtime/annotations/Propagated.java
@@ -5,5 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Propagated {
-    public Class[] value();
+    public PropagationKind[] value();
 }
