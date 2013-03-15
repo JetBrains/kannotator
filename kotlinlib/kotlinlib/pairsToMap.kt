d4248efe11ffbbe273e34009c67a6ae95d489375
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2013-03-15 20:03:40 +0400

    "Pairs to map" update

--- kotlinlib/kotlinlib/pairsToMap.kt
+++ kotlinlib/kotlinlib/pairsToMap.kt
@@ -1,14 +1,16 @@
 package kotlinlib
 
 import java.util.LinkedHashMap
+import java.util.HashMap
 
-public fun <K, V> Iterator<Pair<K, V>>.toMutableMap(): MutableMap<K, V> {
-    val map = LinkedHashMap<K, V>()
+public fun <K, V, M: MutableMap<K, V>> Iterator<Pair<K, V>>.toMutableMap(map: M): M {
     for ((k, v) in this) {
-        map[k] = v
+        map.put(k, v)
     }
     return map
 }
+
+public fun <K, V> Iterator<Pair<K, V>>.toMutableMap(): MutableMap<K, V> = toMutableMap(LinkedHashMap<K, V>())
 
 public fun <K, V> Iterator<Pair<K, V>>.toMap(): Map<K, V> = toMutableMap()
 
@@ -16,4 +18,6 @@ public fun <K, V> Iterable<Pair<K, V>>.toMap(): Map<K, V> = iterator().toMap()
 
 public fun <K, V> Iterable<Pair<K, V>>.toMutableMap(): Map<K, V> = iterator().toMutableMap()
 
+public fun <T, K, V, M: MutableMap<K, V>> Iterable<T>.toMutableMap(map: M, mapper: (T) -> Pair<K, V>): M =
+        iterator().map(mapper).toMutableMap(map)
 public fun <T, K, V> Iterable<T>.toMap(mapper: (T) -> Pair<K, V>): Map<K, V> = iterator().map(mapper).toMap()
