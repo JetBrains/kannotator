c16b008a94eafc47ab5e79e6e90d61422b76a666
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-07-11 22:45:17 +0400

    Removed function that was added to the standard library

--- kotlinlib/kotlinlib/list.kt
+++ kotlinlib/kotlinlib/list.kt
@@ -2,9 +2,6 @@ package kotlinlib
 
 import java.util.Collections
 
-//todo rename other to 'arrayListOf', 'linkedListOf'
-public fun <T> listOf(vararg values: T): List<T> = arrayList(*values)
-
 //not vals for consistency with map
 public fun emptyList(): List<Nothing> = Collections.emptyList()
 public fun emptySet(): Set<Nothing> = Collections.emptySet()
