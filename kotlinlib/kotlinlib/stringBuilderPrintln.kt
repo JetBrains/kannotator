62e1e3aebbdc82c3562bb2b8e46415accb05decc
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2013-11-25 15:18:13 +0400

    Fixes for new inline diagnostics

--- kotlinlib/kotlinlib/stringBuilderPrintln.kt
+++ kotlinlib/kotlinlib/stringBuilderPrintln.kt
@@ -2,43 +2,43 @@ package kotlinlib
 
 val LINE_SEPARATOR: String = System.getProperty("line.separator")!!
 
-public inline fun StringBuilder.println(): StringBuilder {
+public fun StringBuilder.println(): StringBuilder {
     return append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Any?): StringBuilder {
+public fun StringBuilder.println(a: Any?): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Byte): StringBuilder {
+public fun StringBuilder.println(a: Byte): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Short): StringBuilder {
+public fun StringBuilder.println(a: Short): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Int): StringBuilder {
+public fun StringBuilder.println(a: Int): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Long): StringBuilder {
+public fun StringBuilder.println(a: Long): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Float): StringBuilder {
+public fun StringBuilder.println(a: Float): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Double): StringBuilder {
+public fun StringBuilder.println(a: Double): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Char): StringBuilder {
+public fun StringBuilder.println(a: Char): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
-public inline fun StringBuilder.println(a: Boolean): StringBuilder {
+public fun StringBuilder.println(a: Boolean): StringBuilder {
     return append(a).append(LINE_SEPARATOR)
 }
 
