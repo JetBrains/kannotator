f73b2632efd557da22998b2ad569ac38bbaa9e4c
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-12-26 16:44:48 +0400

    initial implementation of automatic generation of jdk annotations

--- commandLineClient/src/org/jetbrains/kannotator/client/jdk.kt
+++ commandLineClient/src/org/jetbrains/kannotator/client/jdk.kt
@@ -16,19 +16,12 @@ import java.util.ArrayList
 import kotlinlib.recurseFiltered
 import kotlinlib.deleteRecursively
 import kotlin.util.measureTimeMillis
+import java.io.PrintStream
 import java.util.Date
+import kotlinlib.invertedRecurse
 
 fun main(args: Array<String>) {
-
-    val start = System.currentTimeMillis()
     annotateJDK()
-    val time = (System.currentTimeMillis() - start) / 1000
-
-    println("time: ${time} secs")
-
-    val now = Date()
-
-    println("$now")
 }
 
 fun annotateJDK() {
@@ -36,44 +29,38 @@ fun annotateJDK() {
     // settings
 
     val jdkJarFile = "lib/jdk/jre-7u12-windows-rt.jar"
-
     val existingAnnotationsDir = File("jdk_custom/jdk-annotations")
-
     val kotlinSignaturesDir = File("jdk_custom/jdk-annotations")
     val outDir = "jdk-annotations-snapshot"
+    val interestingPackages = setOf("java", "javax", "org")
+
+    val outputDir = File(outDir)
+    outputDir.deleteRecursively()
+    outputDir.mkdir()
+
     // annotations for these classes should be included to make class hierarchy consistent
     // from Kotlin point of view
+    // TODO: do we really need it?
     // TODO: incorporate this logic - find such classes in hierarchy and include them automatically
-    val classesToInclude = setOf(
-            "java/lang/AbstractStringBuilder"
-    )
+    val classesToInclude = setOf("java/lang/AbstractStringBuilder")
 
-    val packageFilter = { (name : String) ->
-        name.startsWith("java/") || name.startsWith("javax/") || name.startsWith("org/")
-    }
+    val packageFilter = { (name: String) -> interestingPackages.any { name.startsWith("$it/")} }
 
     val jarSource = FileBasedClassSource(listOf(File(jdkJarFile)))
     val declarationIndex = DeclarationIndexImpl(jarSource)
 
     val propagationOverridesFile = File("jdk_custom/propagationOverrides.txt")
     val nullabilityPropagationOverrides : Annotations<NullabilityAnnotation> =
-            loadAnnotationsFromLogs(arrayListOf(propagationOverridesFile), declarationIndex!!)
+            loadAnnotationsFromLogs(arrayListOf(propagationOverridesFile), declarationIndex)
 
     val conflictExceptions =
             loadPositionsOfConflictExceptions(declarationIndex, File("jdk_custom/exceptions.txt"))
 
-
-    val outputDir = File(outDir)
-    outputDir.deleteRecursively()
-    outputDir.mkdir()
-
-
     val xmlAnnotations = ArrayList<File>()
-    if (existingAnnotationsDir != null) {
+
         existingAnnotationsDir.recurseFiltered({ it.isFile() && it.getName().endsWith(".xml") }, {
             xmlAnnotations.add(it)
         })
-    }
 
     // TODO: try without mutability inferrers
     val inferrers =
@@ -82,8 +69,6 @@ fun annotateJDK() {
                     MUTABILITY_KEY to MUTABILITY_INFERRER as AnnotationInferrer<Any, Qualifier>
             )
 
-    // inferring
-
     val inferenceResult = inferAnnotations(
             classSource = jarSource,
             existingAnnotationFiles = xmlAnnotations,
@@ -91,12 +76,12 @@ fun annotateJDK() {
             errorHandler = NO_ERROR_HANDLING,
             loadOnly = false,
             propagationOverrides =
-                hashMapOf(NULLABILITY_KEY to nullabilityPropagationOverrides, MUTABILITY_KEY to AnnotationsImpl<MutabilityAnnotation>()),
+            hashMapOf(NULLABILITY_KEY to nullabilityPropagationOverrides, MUTABILITY_KEY to AnnotationsImpl<MutabilityAnnotation>()),
             existingAnnotations =
-                hashMapOf(NULLABILITY_KEY to AnnotationsImpl<NullabilityAnnotation>(), MUTABILITY_KEY to AnnotationsImpl<MutabilityAnnotation>()),
-            //packageIsInteresting = packageFilter,
-            packageIsInteresting = {true},
-            existingPositionsToExclude = mapOf()
+            hashMapOf(NULLABILITY_KEY to AnnotationsImpl<NullabilityAnnotation>(), MUTABILITY_KEY to AnnotationsImpl<MutabilityAnnotation>()),
+            packageIsInteresting = packageFilter,
+            existingPositionsToExclude = mapOf(),
+            progressMonitor = JDKProgressIndicator()
     )
 
     val nullability: InferenceResultGroup<NullabilityAnnotation>  =
@@ -120,10 +105,77 @@ fun annotateJDK() {
             kotlinSignaturesDir = kotlinSignaturesDir,
             destRoot = outputDir,
             nullability = nullabilityInferred,
-            propagatedNullabilityPositions = setOf(), // we do not store this information in jdk annotations
+            propagatedNullabilityPositions = nullability.propagatedPositions,
             includedClassNames = classesToInclude,
             errorHandler = simpleErrorHandler {
                 kind, message ->
                 throw IllegalArgumentException(message)
             })
+
+    // current annotations writers dump all packages, including not interesting
+    // deleting not interesting packages
+    outputDir.invertedRecurse { file ->
+        val path = file.getPath()
+        val interestingFolders = interestingPackages.map {"$outDir/$it"}
+        val interesting = interestingFolders.any { path == it || path.startsWith("$it/") }
+        if (!interesting) {
+            file.delete()
+        }
+    }
 }
+
+class JDKProgressIndicator() : FileAwareProgressMonitor() {
+
+    var numberOfMethods = 0
+    var numberOfProcessedMethods = 0
+    var progressPercent = -1
+
+    fun logVerbose(msg: String) {
+        println("${Date()} ${msg}")
+    }
+
+    override fun processingStarted() {
+        logVerbose("Started...")
+        numberOfProcessedMethods = 0
+    }
+
+    override fun annotationIndexLoaded(index: AnnotationKeyIndex) {
+        logVerbose("Index loaded")
+    }
+
+    override fun methodsProcessingStarted(methodCount: Int) {
+        numberOfMethods = methodCount
+        logVerbose("Starting methods ${methodCount}")
+    }
+
+    override fun processingComponentFinished(methods: Collection<Method>) {
+        numberOfProcessedMethods += methods.size
+
+        if (numberOfMethods != 0) {
+            val currentProgressPercent = (numberOfProcessedMethods.toDouble() / numberOfMethods * 100).toInt()
+            if (currentProgressPercent != progressPercent) {
+                progressPercent = currentProgressPercent
+                logVerbose("  Inferring: $currentProgressPercent% \r");
+            }
+        } else {
+            logVerbose("  Inferring: 100% \n");
+        }
+    }
+
+    override fun processingAborted() {
+        logVerbose("Annotating was canceled")
+    }
+
+    //All libraries are annotated
+    override fun allFilesAreAnnotated() {
+        logVerbose("Annotating finished successfully: ")
+    }
+
+    override fun jarProcessingStarted(fileName: String, libraryName: String) {
+        logVerbose("Processing $fileName")
+    }
+
+    override fun jarProcessingFinished(fileName: String, libraryName: String) {
+    }
+}
+
