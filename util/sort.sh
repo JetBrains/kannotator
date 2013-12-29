7d78b305e644a33546528ce0b322f66823d5a938
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-12-29 15:47:30 +0400

    putting delta of annotations into one file with comments, a list of removed annotations (with comments why they were removed), utilities for sorting xml are put into the same folder

--- util/sort.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf compare/jdk-annotations-snapshot
-rm -rf compare/jdk-annotations
-find jdk-annotations -type f -exec xsltproc --output compare/"{}" util/sort.xml "{}" \;
-find jdk-annotations-snapshot -type f -exec xsltproc --output compare/"{}" util/sort.xml "{}" \;
-rm -rf compare/jdk-annotations-snapshot/com
-rm -rf compare/jdk-annotations-snapshot/sun
