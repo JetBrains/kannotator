787409d8b98de0cdea377c59bdc878301f533b8c
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-12-29 13:02:16 +0400

    minor: removing only current folders

--- util/sort.sh
+++ util/sort.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm -rf compare/*
+rm -rf compare/jdk-annotations-snapshot
+rm -rf compare/jdk-annotations
 find jdk-annotations -type f -exec xsltproc --output compare/"{}" util/sort.xml "{}" \;
 find jdk-annotations-snapshot -type f -exec xsltproc --output compare/"{}" util/sort.xml "{}" \;
 rm -rf compare/jdk-annotations-snapshot/com
