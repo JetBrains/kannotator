cc04d2f52fd6f47d198e55e88195dd552f4759fd
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-01-20 15:44:03 +0400

    test utility for loading annotations from byte code

--- test/util/util.kt
+++ test/util/util.kt
@@ -1,15 +1,25 @@
 package util
 
 import java.io.File
-import org.jetbrains.kannotator.declarations.ClassName
+import java.util.ArrayList
+
+import junit.framework.Assert
+
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.Type
-import org.jetbrains.kannotator.util.processJar
-import java.util.ArrayList
+
 import kotlinlib.recurseFiltered
-import org.jetbrains.kannotator.index.ClassSource
-import junit.framework.Assert
 import kotlinlib.toUnixSeparators
+
+import org.jetbrains.kannotator.NO_ERROR_HANDLING
+import org.jetbrains.kannotator.annotationsInference.nullability.NullabilityAnnotation
+import org.jetbrains.kannotator.controlFlow.builder.analysis.NULLABILITY_KEY
+import org.jetbrains.kannotator.declarations.ClassName
+import org.jetbrains.kannotator.declarations.Annotations
+import org.jetbrains.kannotator.index.ClassSource
+import org.jetbrains.kannotator.index.DeclarationIndexImpl
+import org.jetbrains.kannotator.main.inferAnnotations
+import org.jetbrains.kannotator.util.processJar
 
 fun recurseIntoJars(libDir: File, block: (jarFile: File, classType: Type, classReader: ClassReader) -> Unit) {
     libDir.recurse {
@@ -74,4 +84,19 @@ fun assertEqualsOrCreate(expectedFile: File, actual: String, failOnNoData: Boole
 
     Assert.assertEquals(expected.toUnixSeparators(), actual.toUnixSeparators())
     return true
+}
+
+fun loadNullabilityAnnotations(classSource: ClassSource): Annotations<NullabilityAnnotation> {
+    val inferenceResult = inferAnnotations(
+            classSource = classSource,
+            existingAnnotationFiles = listOf(),
+            inferrers = INFERRERS,
+            errorHandler = NO_ERROR_HANDLING,
+            loadOnly = true,
+            propagationOverrides = mapOf(),
+            existingAnnotations = mapOf(),
+            existingPositionsToExclude = mapOf(),
+            packageIsInteresting = {true}
+    )
+    return inferenceResult.groupByKey[NULLABILITY_KEY]!!.existingAnnotations as Annotations<NullabilityAnnotation>
 }
