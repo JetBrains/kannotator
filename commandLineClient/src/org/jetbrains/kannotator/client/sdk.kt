583cecc6dae160fa0c409ad1352e86ffdf0aed0e
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-02-17 12:34:42 +0400

    single ant script for jdk and android annotations

--- commandLineClient/src/org/jetbrains/kannotator/client/sdk.kt
+++ commandLineClient/src/org/jetbrains/kannotator/client/sdk.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.kannotator.client.jdk
+package org.jetbrains.kannotator.client.sdk
 
 import org.jetbrains.kannotator.*
 import org.jetbrains.kannotator.annotations.io.*
@@ -23,45 +23,69 @@ import java.io.FileReader
 import java.io.BufferedReader
 
 fun main(args: Array<String>) {
-    annotateJDK()
+    val jarFile = File(args[0])
+    val inDir = File(args[1])
+
+    val outputDir = File(args[2])
+    val jaifName = args[3]
+
+    outputDir.deleteRecursively()
+    outputDir.mkdir()
+    annotateSDK(jarFile, inDir, outputDir, jaifName)
 }
 
-fun annotateJDK() {
+/* annotates SDK (JDK or android sdk)
+ * jarFile - bytecode to annotated
+ * inDir - directory with customization information (see below)
+ * outputDir - directory where annotations in xml format will be placed
+ * jaifName - jaif will be out/artifacts/annotations/${jaifName}.jaif
+ *
+ * this script assumes that customization dir has following (see jdk-custom/README.md):
+ * - interestingPackages.txt
+ * - includedClassNames.txt
+ * - propagationOverrides.txt
+ * - exceptions.txt
+ * - annotations/...
+ */
+fun annotateSDK(jarFile: File, inDir: File, outputDir: File, jaifName: String) {
+    val jarSource = FileBasedClassSource(listOf(jarFile))
+    val declarationIndex = DeclarationIndexImpl(jarSource)
 
     // settings
-
-    val jdkJarFile = "lib/jdk/jre-7u12-windows-rt.jar"
-    val existingAnnotationsDir = File("jdk-custom/jdk-annotations")
-    val kotlinSignaturesDir = File("jdk-custom/jdk-annotations")
-    val outDir = "jdk-annotations-snapshot"
-    val interestingPackages = setOf("java", "javax", "org")
-
     val outDirJAIF = File("out/artifacts/annotations")
     outDirJAIF.mkdirs()
 
-    val outputDir = File(outDir)
-    outputDir.deleteRecursively()
-    outputDir.mkdir()
+    val existingAnnotationsDir = File(inDir, "annotations")
+    val kotlinSignaturesDir = File(inDir, "annotations")
 
-    // annotations for these classes should be included to make class hierarchy consistent
-    // from Kotlin point of view
-    // TODO: do we really need it?
-    // TODO: incorporate this logic - find such classes in hierarchy and include them automatically
-    val includedClassNames = BufferedReader(FileReader(File("jdk-custom/includedClassNames.txt"))) use { p ->
-        p.lineIterator().toSet()
-    }
+    val interestingPackagesFile = File(inDir, "interestingPackages.txt")
+    val includedClassNamesFile = File(inDir, "includedClassNames.txt")
+    val propagationOverridesFile = File(inDir, "propagationOverrides.txt")
+    val exceptionsFile = File(inDir, "exceptions.txt")
 
-    val packageFilter = { (pkg: String) -> interestingPackages.any { interestingPkg -> pkg == interestingPkg || pkg.startsWith("$interestingPkg/")} }
+    val interestingPackages =
+            if (interestingPackagesFile.exists())
+                BufferedReader(FileReader(interestingPackagesFile)) use { p ->p.lineIterator().toSet() }
+            else
+                setOf<String>()
 
-    val jarSource = FileBasedClassSource(listOf(File(jdkJarFile)))
-    val declarationIndex = DeclarationIndexImpl(jarSource)
+    val includedClassNames =
+            if (includedClassNamesFile.exists())
+                BufferedReader(FileReader(includedClassNamesFile)) use { p -> p.lineIterator().toSet()}
+            else
+                setOf<String>()
 
-    val propagationOverridesFile = File("jdk-custom/propagationOverrides.txt")
+    val packageFilter =
+            if (interestingPackages.empty)
+                {(pkg: String) -> true}
+            else
+                {(pkg: String) -> interestingPackages.any { interestingPkg -> pkg == interestingPkg || pkg.startsWith("$interestingPkg/")} }
+
     val nullabilityPropagationOverrides : Annotations<NullabilityAnnotation> =
             loadAnnotationsFromLogs(arrayListOf(propagationOverridesFile), declarationIndex)
 
     val conflictExceptions =
-            loadPositionsOfConflictExceptions(declarationIndex, File("jdk-custom/exceptions.txt"))
+            loadPositionsOfConflictExceptions(declarationIndex, exceptionsFile)
 
     val xmlAnnotations = ArrayList<File>()
 
@@ -69,8 +93,6 @@ fun annotateJDK() {
         xmlAnnotations.add(it)
     })
 
-    // TODO: try without mutability inferrers - possibly faster inference,
-    // does mutability inference really help to infer nullability in the case of jdk annotations?
     val inferrers =
             mapOf<AnalysisType, AnnotationInferrer<Any, Qualifier>>(
                     NULLABILITY_KEY to NullabilityInferrer() as AnnotationInferrer<Any, Qualifier>,
@@ -89,7 +111,7 @@ fun annotateJDK() {
             hashMapOf(NULLABILITY_KEY to AnnotationsImpl<NullabilityAnnotation>(), MUTABILITY_KEY to AnnotationsImpl<MutabilityAnnotation>()),
             packageIsInteresting = packageFilter,
             existingPositionsToExclude = mapOf(),
-            progressMonitor = JDKProgressIndicator()
+            progressMonitor = SDKProgressIndicator()
     )
 
     val nullability: InferenceResultGroup<NullabilityAnnotation>  =
@@ -108,7 +130,7 @@ fun annotateJDK() {
     writeAnnotationsToJaif(
             declarationIndex,
             destRoot = outDirJAIF,
-            fileName = "kotlin-jdk-annotations",
+            fileName = jaifName,
             nullability = nullability.inferredAnnotations,
             propagatedNullabilityPositions = nullability.propagatedPositions,
             includeNullable = true
@@ -140,7 +162,7 @@ fun annotateJDK() {
             """)
 }
 
-class JDKProgressIndicator() : FileAwareProgressMonitor() {
+class SDKProgressIndicator() : FileAwareProgressMonitor() {
 
     var numberOfMethods = 0
     var numberOfProcessedMethods = 0
